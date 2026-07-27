dd58684660f9f32de41be6f60689420e4c825ee8
NyAro30 <nyarorasolonjanaharymandimby@gmail.com>
2026-07-28 02:46:16 +0300

    mypy --strcit fixed

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -7,18 +7,18 @@
 #   By: mny-aro- <mny-aro-@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/07/27 23:05:09 by mny-aro-            #+#    #+#            #
-#   Updated: 2026/07/28 02:18:15 by mny-aro-           ###   ########.fr      #
+#   Updated: 2026/07/28 02:45:10 by mny-aro-           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 import typing
 from abc import ABC, abstractmethod
 
-class DataProcessor(ABC):
-    def __init__(self):
-        self.rank = 0
-        self.data : list[tuple[int, str]] = []
 
+class DataProcessor(ABC):
+    def __init__(self) -> None:
+        self.rank: int = 0
+        self.data: list[tuple[int, str]] = []
 
     def count_rank(self) -> int:
         self.rank += 1
@@ -31,10 +31,11 @@ class DataProcessor(ABC):
     @abstractmethod
     def ingest(self, data: typing.Any) -> None:
         pass
-    
+
     def output(self) -> tuple[int, str]:
         collect = self.data.pop(0)
         return collect
+
 
 class NumericProcessor(DataProcessor):
     def validate(self, data: typing.Any) -> bool:
@@ -47,26 +48,26 @@ class NumericProcessor(DataProcessor):
         else:
             return False
 
-    def ingest(self, data: int | float |
-               list[int | float]) -> None:
+    def ingest(self, data: int | float
+               | list[int | float]) -> None:
         if not self.validate(data):
             raise ValueError("Improper numeric data")
         if isinstance(data, list):
             for item in data:
                 rank = self.count_rank()
-                item = str(item)
-                self.data.append((rank, item))
+                str_item = str(item)
+                self.data.append((rank, str_item))
         elif isinstance(data, (int, float)):
             rank = self.count_rank()
             value = str(data)
-            self.data.append((rank, value))                
+            self.data.append((rank, value))
 
 
 class TextProcessor(DataProcessor):
     def validate(self, data: typing.Any) -> bool:
         if isinstance(data, list):
             is_str_list = all(isinstance(element, str)
-                for element in data)
+                              for element in data)
             return is_str_list
         elif isinstance(data, str):
             return True
@@ -93,13 +94,13 @@ class LogProcessor(DataProcessor):
             keys_ok = all(isinstance(k, str)
                           for k in d.keys())
             values_ok = all(isinstance(v, str)
-                          for v in d.values())
+                            for v in d.values())
             return keys_ok and values_ok
 
         if isinstance(data, list):
             return all(is_valid_dict(item)
                        for item in data)
-        elif isinstance(data,dict):
+        elif isinstance(data, dict):
             return is_valid_dict(data)
         else:
             return False
@@ -128,7 +129,7 @@ def main() -> None:
     log = LogProcessor()
 
     print("\nTesting Numeric Processor...")
-    nbr = [42, "Hello"]
+    nbr: list[int | str] = [42, "Hello"]
     for test in nbr:
         print(f" Trying to validate input '{test}':"
               f" {numeric.validate(test)}")
@@ -138,30 +139,30 @@ def main() -> None:
         numeric.ingest("foo")
     except Exception as err:
         print(f" Got exception: {err}")
-    valid_list = [1, 2, 3, 4, 5]
+    valid_list: list[int | float] = [1, 2, 3, 4, 5]
     print(f" Processing data: {valid_list}")
     numeric.ingest(valid_list)
     to_extract1 = 3
     if to_extract1 == 1:
-        print(f" Exctracting {to_extract1} value...")
+        print(f" Extracting {to_extract1} value...")
     else:
-        print(f" Exctracting {to_extract1} values...")
+        print(f" Extracting {to_extract1} values...")
     for _ in range(to_extract1):
         rank, value = numeric.output()
         print(f" Numeric value {rank}: {value}")
 
 
     print("\nTesting Text Processor...")
-    new_list = [42, ['Hello', 'Nexus', 'World']]
-    print(f" Trying to validate input '{nbr[0]}':"
-          f"{text.validate(new_list[0])}")
-    print(f" Processing data: {new_list[1]}")
-    text.ingest(new_list[1])
+    print(f" Trying to validate input '42':"
+          f" {text.validate(42)}")
+    text_data: list[str] = ["Hello", "Nexus", "World"]
+    print(f" Processing data: {text_data}")
+    text.ingest(text_data)
     to_extract2 = 1
     if to_extract2 == 1:
-        print(f" Exctracting {to_extract2} value...")
+        print(f" Extracting {to_extract2} value...")
     else:
-        print(f" Exctracting {to_extract2} values...")
+        print(f" Extracting {to_extract2} values...")
     for _ in range(to_extract2):
         rank, value = text.output()
         print(f" Text value {rank}: {value}")
@@ -170,12 +171,7 @@ def main() -> None:
     print("\nTesting Log Processor...")
     print(f" Trying to validate input 'Hello':"
           f" {log.validate('Hello')}")
-    print(" Processing data: [{'log_level': 'NOTICE',"
-          " 'log_message': 'Connection to server'},"
-          " {'log_level': 'ERROR    "
-          "      '. 'log_message': "
-          "'Unauthorized access!!'}]")
-    log.ingest([
+    log_data: list[dict[str, str]] = [
         {
             "log_level": "NOTICE",
             "log_message": "Connection to server",
@@ -184,7 +180,9 @@ def main() -> None:
             "log_level": "ERROR",
             "log_message": "Unauthorized access!!",
         },
-    ])
+    ]
+    print(f" Processing data: {log_data}")
+    log.ingest(log_data)
     to_extract3 = 2
     if to_extract3 == 1:
         print(f" Extracting {to_extract3} value...")
